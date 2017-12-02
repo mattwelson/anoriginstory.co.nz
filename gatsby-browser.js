4d68bbd781077d215eccc9d79e43971369e88464
Matt Welson <matthew.welson@gmail.com>
2017-12-03 10:28:35 +1300

    restricting parallax to a certain width (as a const)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,24 @@
 import throttle from 'lodash/throttle'
 
-const strength = 0.1
+const strength = 0.12
+const minWidth = 700
 
 exports.onInitialClientRender = () => {
   window.addEventListener(
     'scroll',
     throttle(
       _ => {
-        const sections = [
-          ...document.querySelectorAll('.section__hero--parallax')
-        ]
+        if (window.innerWidth > minWidth) {
+          const sections = [
+            ...document.querySelectorAll('.section__hero--parallax')
+          ]
 
-        sections[0].forEach(s => {
-          const delta = (window.pageYOffset - s.offsetTop) * strength
-          const image = s.querySelector('img')
-          image.setAttribute('style', `transform: translateY(${delta}px)`)
-        })
+          sections[0].forEach(s => {
+            const delta = (window.pageYOffset - s.offsetTop) * strength
+            const image = s.querySelector('img')
+            image.setAttribute('style', `transform: translateY(${delta}px)`)
+          })
+        }
       },
       15,
       { leading: true }
